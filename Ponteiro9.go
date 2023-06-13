470d8021525a83921448a483c03654289dd40d84
ericpro2004 <126572065+ericpro2004@users.noreply.github.com>
2023-06-13 17:27:25 -0300

    Update Ponteiro9.go

--- Ponteiro9.go
+++ Ponteiro9.go
@@ -1,1 +1,30 @@
+package main
 
+import (
+	"fmt"
+)
+
+type Livro struct {
+	Título string
+	Autor  string
+	Preço  float64
+}
+
+func aplicarDesconto(livro *Livro) {
+	desconto := 0.1 // 10% de desconto
+	livro.Preço -= livro.Preço * desconto
+}
+
+func main() {
+	livro := Livro{
+		Título: "Aventuras no Espaço",
+		Autor:  "João Silva",
+		Preço:  50.0,
+	}
+
+	fmt.Printf("Preço antes do desconto: R$ %.2f\n", livro.Preço)
+
+	aplicarDesconto(&livro)
+
+	fmt.Printf("Preço após o desconto: R$ %.2f\n", livro.Preço)
+}
